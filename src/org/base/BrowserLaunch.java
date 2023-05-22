38a1c5ed9620805becb2e68a77cb06ce23e60017
kkelvin93 <kelvinstein007@gmail.com>
2023-05-22 14:33:00 +0530

    LocalToRemote

---
+++ src/org/base/BrowserLaunch.java
@@ -0,0 +1,24 @@
+package org.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\KELVIN KAVIN\\eclipse-workspace\\GITHub01c180523\\driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.facebook.com/");
+		
+		System.out.println("Browser launched successfully");
+		
+
+	}
+
+}
